f15058e7a7906de72087bb157cce33d6b27c27f5
yowell <guoyouy@126.com>
2018-01-25 18:05:02 +0800

    unserialize

---
+++ unserialize/config.php
@@ -0,0 +1,8 @@
+<?php
+    $db_host = 'localhost';
+    $db_name = 'flag';
+    $db_user = 'root';
+    $db_pass = '123';
+    $DEBUG = @$_GET['noggnogg'];
+    $FLAG = "HITCON{php 4nd mysq1 are s0 mag1c, isn't it?}";
+?>
